73eec800545534181ab8a13ea181ce0092485919
tyhuynh99 <tyhuynh1199@gmail.com>
2020-12-28 17:48:57 +0700

    test server 2

--- PromotionEngineAPI/Controllers/WeatherForecastController.cs
+++ PromotionEngineAPI/Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace PromotionEngineAPI.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "1", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "1", "Bracing_1", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
