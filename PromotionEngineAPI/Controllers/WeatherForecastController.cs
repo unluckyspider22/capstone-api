3bf7a571215db312c474481d3c57d3dbdc8e7339
Nguyễn Trung Nam <namnt>
2020-12-28 17:15:28 +0700

    test server

--- PromotionEngineAPI/Controllers/WeatherForecastController.cs
+++ PromotionEngineAPI/Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace PromotionEngineAPI.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "1", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
