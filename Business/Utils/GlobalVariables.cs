1080417680a6bcc8800463df57d0f84e3664862a
Nguyễn Trung Nam <namnt>
2020-12-29 13:53:20 +0700

    change delflg int -> string

---
+++ Business/Utils/GlobalVariables.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApplicationCore.Utils
+{
+    public static class GlobalVariables
+    {
+        public const int SUCCESS = 1;
+        public const int FAIL = 0;
+        public const int NOT_FOUND = -1;
+        public const int DUPLICATE = 2;
+        public const string DELETED = "1";
+    }
+}
