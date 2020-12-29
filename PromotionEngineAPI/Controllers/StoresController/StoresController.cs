179cc8be2587912f20694384e4754220d68691b6
Do Nguyen <donguyen.higashi@gmail.com>
2020-12-29 21:56:08 +0700

    aaa

--- PromotionEngineAPI/Controllers/StoresController/StoresController.cs
+++ PromotionEngineAPI/Controllers/StoresController/StoresController.cs
@@ -13,62 +13,74 @@ namespace PromotionEngineAPI.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-        public class StoresController : ControllerBase
+    public class StoresController : ControllerBase
+    {
+        private readonly IStoreService _service;
+
+        public StoresController(IStoreService service)
         {
-            private readonly IStoreService _service;
+            _service = service;
+        }
 
-            public StoresController(IStoreService service)
-            {
-                _service = service;
-            }
-
-            // GET: api/Stores
-            [HttpGet]
-            public List<Store> GetStore()
-            {
+        // GET: api/Stores
+        [HttpGet]
+        public List<Store> GetStore()
+        {
             var result = _service.GetStores();
-                return result;
-            }
+            return result;
+        }
 
-            // GET: api/Stores/5
-            [HttpGet("{id}")]
-            public ActionResult<StoreParam> GetStore(Guid id)
-            {
-            var result = _service.GetStore(id);
+        // GET: api/Stores/5
+        [HttpGet("{id}")]
+        public ActionResult<StoreParam> GetStore(string id)
+        {
+            System.Guid param = new System.Guid(id);
+            StoreParam result = _service.GetStore(param);
             if (result == null) return NotFound();
-                return Ok(result);
-            }
+            return Ok(result);
+        }
 
-            // PUT: api/Stores/5
-            // To protect from overposting attacks, please enable the specific properties you want to bind to, for
-            // more details see https://aka.ms/RazorPagesCRUD.
-            [HttpPut]
-            public ActionResult<Store> PutStore(Store store)
-            {
-                var result = _service.PutStore(store);
-                if (result == GlobalVariables.NOT_FOUND) return NotFound();
-                return Ok(store);
-            }
+        // PUT: api/Stores/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        [HttpPut]
+        public ActionResult<Store> PutStore(Store store)
+        {
+            var result = _service.PutStore(store);
+            if (result == GlobalVariables.NOT_FOUND) return NotFound();
+            return Ok(store);
+        }
 
-            // POST: api/Stores
-            // To protect from overposting attacks, please enable the specific properties you want to bind to, for
-            // more details see https://aka.ms/RazorPagesCRUD.
-            [HttpPost]
-            public ActionResult<Store> PostStore(Store store)
-            {
-                var result = _service.PostStore(store);
-                if (result == GlobalVariables.DUPLICATE) return Conflict();
-                return Ok(store);
-            }
+        // POST: api/Stores
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        [HttpPost]
+        public ActionResult<Store> PostStore(Store store)
+        {
+            var result = _service.PostStore(store);
+            if (result == GlobalVariables.DUPLICATE) return Conflict();
+            return Ok(store);
+        }
 
-            // DELETE: api/Stores/5
-            [HttpDelete("{id}")]
-            public ActionResult DeleteStore(Guid id)
-            {
-            var result = _service.DeleteStore(id);
-                if (result == GlobalVariables.NOT_FOUND) return NotFound();
+        // DELETE: api/Stores/5
+        [HttpDelete("{id}")]
+        public ActionResult DeleteStore(string id)
+        {
+            System.Guid param = new System.Guid(id);
+            var result = _service.DeleteStore(param);
+            if (result == GlobalVariables.NOT_FOUND) {
                 return Ok();
             }
+            return Ok();
+        }
+
+        [HttpGet]
+        [Route("count")]
+        public ActionResult GetStoreCount()
+        {
+            var result = _service.CountStore();
+            return Ok(result);
         }
     }
+}
 
