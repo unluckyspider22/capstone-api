af626c648296fcdff944e8f48e13ed8d32dd919d
Nguyễn Trung Nam <namnt>
2020-12-30 17:35:14 +0700

    tạo database mới bởi vì database cũ hết tiền rồi. Lêu lêu tụi đỗ nghèo khỉ

--- Data/Models/PromotionEngineContext.cs
+++ Data/Models/PromotionEngineContext.cs
@@ -38,7 +38,7 @@ namespace Infrastructure.Models
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseSqlServer("Server=tcp:promteng.database.windows.net,1433;Initial Catalog=PromotionEngine;Persist Security Info=False;User ID=adm;Password=Abcd1234;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+                optionsBuilder.UseSqlServer("Server=tcp:promotionengine.database.windows.net,1433;Initial Catalog=PromotionEngine;Persist Security Info=False;User ID=adm;Password=Abcd1234;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
             }
         }
 
