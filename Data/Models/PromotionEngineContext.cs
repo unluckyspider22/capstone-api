da56b7ac2c5dca984102fca00c43858f1e6b45f9
tyhuynh99 <tyhuynh1199@gmail.com>
2021-01-03 20:18:22 +0700

    remove local db connection

--- Data/Models/PromotionEngineContext.cs
+++ Data/Models/PromotionEngineContext.cs
@@ -38,8 +38,8 @@ namespace Infrastructure.Models
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseSqlServer("Server=.,1432;Initial Catalog=PromotionEngine;Persist Security Info=False;User ID=sa;Password=tyhuynh99; MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=True;");
-                //optionsBuilder.UseSqlServer("Server=tcp:promotionengine.database.windows.net,1433;Initial Catalog=PromotionEngine;Persist Security Info=False;User ID=adm;Password=Abcd1234;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+                //optionsBuilder.UseSqlServer("Server=.,1432;Initial Catalog=PromotionEngine;Persist Security Info=False;User ID=sa;Password=tyhuynh99; MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=True;");
+                optionsBuilder.UseSqlServer("Server=tcp:promotionengine.database.windows.net,1433;Initial Catalog=PromotionEngine;Persist Security Info=False;User ID=adm;Password=Abcd1234;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
             }
         }
 
