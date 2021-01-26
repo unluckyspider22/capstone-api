4cad5766c284750558eacc1bcf4598539554f614
Tỷ Huỳnh <tyhuynh1199@gmail.com>
2021-01-26 15:09:51 +0700

    hot fix - update model

--- Data/Models/Action.cs
+++ Data/Models/Action.cs
@@ -6,7 +6,6 @@ namespace Infrastructure.Models
     public partial class Action
     {
         public Guid ActionId { get; set; }
-        public Guid? PromotionTierId { get; set; }
         public string ActionType { get; set; }
         public bool IsLimitAmount { get; set; }
         public string ProductCode { get; set; }
