e93b35433b6198479f1a17972bb6e583b0afac9b
Do Nguyen <donguyen.higashi@gmail.com>
2021-02-23 16:34:39 +0700

    test

--- Business/Chain/TimeframeHandle.cs
+++ Business/Chain/TimeframeHandle.cs
@@ -1,8 +1,11 @@
 ﻿using ApplicationCore.Request;
 using Infrastructure.DTOs;
+using Infrastructure.Helper;
+using Infrastructure.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 
 namespace ApplicationCore.Chain
@@ -18,6 +21,13 @@ namespace ApplicationCore.Chain
             var promotions = order.Promotions;
 
             base.Handle(order);
+        }
+        public void HandleExpiredDate(Promotion promotion, OrderResponseModel order)
+        {
+            if(order.OrderDetail.BookingDate.CompareTo(promotion.StartDate)<0 && order.OrderDetail.BookingDate.CompareTo(promotion.EndDate) > 0)
+            {
+                throw new ErrorObj(code: (int)HttpStatusCode.BadRequest, message: AppConstant.ErrMessage.Invalid_Time);
+            }
         }
     }
 }
