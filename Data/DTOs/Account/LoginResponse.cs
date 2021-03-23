6fe6e7bc27a2efd06dc069a2a96c04c24901f837
Nguyễn Trung Nam <namnt>
2021-03-23 08:57:51 +0700

    get user name

--- Data/DTOs/Account/LoginResponse.cs
+++ Data/DTOs/Account/LoginResponse.cs
@@ -14,6 +14,7 @@ namespace Infrastructure.DTOs.Account
     }
     public class UserInfo
     {
+        public string Username { get; set; }
         public string Token { get; set; }
         public string BrandCode { get; set; }
         public Guid BrandId { get; set; }
