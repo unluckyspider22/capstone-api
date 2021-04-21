9246e490b5850c84fe6b232232e4ce20c2b13dc1
Tỷ Huỳnh <tyhuynh1199@gmail.com>
2021-04-21 13:47:19 +0700

    fix hard code

--- Data/Helper/AppConstant.cs
+++ Data/Helper/AppConstant.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
-using System.Threading.Tasks;
 
 namespace Infrastructure.Helper
 {
@@ -31,9 +30,9 @@ namespace Infrastructure.Helper
         public const int RSA_LENGTH_2048 = 2048;
         public enum ChannelType
         {
-            In_Store=1,
-            Online=2,
-            Other=3
+            In_Store = 1,
+            Online = 2,
+            Other = 3
         }
         public const string Sender = "Promotion Engine";
         public const string Sender_Email = "promotion.engine.fpt@gmail.com";
@@ -192,12 +191,12 @@ namespace Infrastructure.Helper
             Login_Fail = 140,
             Device_Access_Fail = 141,
             Device_Access_Server_Fail = 142,
-            Exist_ProductCategory=143,
+            Exist_ProductCategory = 143,
             MemberLevel_Exist = 144,
             Product_Exist = 145,
-            Product_Cate_NotFound=146,
-            MemberLevel_NotFound=147,
-            Order_Fail=148,
+            Product_Cate_NotFound = 146,
+            MemberLevel_NotFound = 147,
+            Order_Fail = 148,
             Internal_Server_Error = 500
         }
         public class ErrMessage
@@ -379,5 +378,6 @@ namespace Infrastructure.Helper
             public const int USED = 3;
             public const int REDEMPED = 4;
         }
+        public const string CONNECTION_STRING = "Server=tcp:promotionengine.database.windows.net,1433;Database=PromotionEngine;User ID=adm;Password=Abcd1234;Trusted_Connection=false;MultipleActiveResultSets=true";
     }
 }
